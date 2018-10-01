ae3467ebe2a5e8a462a9d7e67b88cd4cde5e6176
Will Reichl <William.Reichl@dcsg.com>
2018-09-30 23:04:30 -0400

    S2L20 - Will - Exercise 2 Solution

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,23 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Section 2: Lecture 12 - Defining a custom route
+            // Problems with this approach
+            // 1. This file eventually fills up with a lot of custom routes
+            // 2. Have to go back and forth between actions and custom routes
+            // 3. Automatic refactoring/renaming in Controller does not work in route -- code becomes fragile
+            /*
+            routes.MapRoute(
+                name: "MoviesByReleaseDate", 
+                url: "movies/released/{year}/{month}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate" },
+                constraints: new { year = @"\d{4}", month = @"\d{2}" }
+            );
+            */
+
+            //Section 2: Lection 13 - Attribute Routing
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -7,6 +7,23 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-E" }
+            };
+        }
+
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -23,7 +40,35 @@ namespace Vidly.Controllers
                 Customers = customers
             };
 
+            //return View(viewModel); // One method of passing data to the view, is via an argument, preferred way
+            //behind the scenes, this uses:
+            //viewResult.ViewData.Model
+
+
+            //2nd method - View Dictionary
+            /*
+            ViewData["Movie"] = movie;
+            return View();
+
+            Downside to this method, (1) view code is ugly (must cast), and (2) view code is dependent on "magic string" of "Movie", makes code fragile
+            */
+
+            //3rd method -ViewBag, instead of "magic string", you have "magic object", still fragile
+            //ViewBag.Movie = movie;
+            //return View();
+
             return View(viewModel);
+        }
+
+        // Section 2: Lesson 13 - Using attribute routing w/ constraints
+        /*
+         * Other possible constraints include min, max, minlength, maxlength, int, float, guid
+         * 
+         */
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
