66f04be9e3abd14cc826bb5031ade9dba104439a
Will Reichl <William.Reichl@dcsg.com>
2018-10-03 09:38:24 -0400

    S4L39 - Labels - Using "Display" annotation in model to create user-friendly labels for form input fields.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -17,7 +17,7 @@ namespace Vidly.Models
         public MembershipType MembershipType { get; set; }
 
         public byte MembershipTypeId { get; set; }
-
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
     }
 }
