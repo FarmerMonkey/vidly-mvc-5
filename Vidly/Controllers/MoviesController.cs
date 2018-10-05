f2d55a61e1931c39a793e66666b84e37dd1dd0cc
Will Reichl <William.Reichl@dcsg.com>
2018-10-05 15:50:09 -0400

    S5L60 - Synchronized my Movie solution with Mosh's so that it would match for future lessons and exercises.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,10 +1,10 @@
-﻿using System.Linq;
-using System.Data.Entity;
+﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
-using System;
 
 namespace Vidly.Controllers
 {
@@ -24,89 +24,48 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            var movies = _context.Movies.Include(c => c.Genre).ToList();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        public ActionResult Details(int id)
+        public ViewResult New()
         {
-            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            var genres = _context.Genres.ToList();
 
-            if (movie == null)
-                return HttpNotFound();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
 
-            return View(movie);
+            return View("MovieForm", viewModel);
         }
 
         public ActionResult Edit(int id)
         {
             var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
             if (movie == null)
                 return HttpNotFound();
 
-            var genres = _context.Genres.ToList();
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Genres = genres,
-                Movie = movie
+                Genres = _context.Genres.ToList()
             };
 
             return View("MovieForm", viewModel);
         }
 
-        public ActionResult New()
+
+        public ActionResult Details(int id)
         {
-            var genres = _context.Genres.ToList();
-            var viewModel = new MovieFormViewModel
-            {
-                Genres = genres,
-                Movie = new Movie()
-            };
-            return View("MovieForm", viewModel);
-        }
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
+            if (movie == null)
+                return HttpNotFound();
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Save(Movie movie)
-        {
-            if (!ModelState.IsValid)
-            {
-                var viewModel = new MovieFormViewModel
-                {
-                    Movie = movie,
-                    Genres = _context.Genres.ToList()
-                };
-                return View("MovieForm", viewModel);
-            }
+            return View(movie);
 
-            if (movie.Id == 0)
-            {
-                movie.DateAdded = DateTime.Now;
-                _context.Movies.Add(movie);
-            }
-            else
-            {
-                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
-                movieInDb.Name = movie.Name;
-                movieInDb.ReleaseDate = movie.ReleaseDate;
-                movieInDb.GenreId = movie.GenreId;
-                movieInDb.NumberInStock = movie.NumberInStock;
-            }
-            _context.SaveChanges(); // persist changes to database -- context tracks DML type (ins/upd/del) and generates DML statements at runtime
-
-            // there is no "Create" view associated with this action, instead, we'll redirect users back to list of customers (index)
-            return RedirectToAction("Index", "Movies");
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-E" }
-            };
         }
 
 
@@ -126,35 +85,40 @@ namespace Vidly.Controllers
                 Customers = customers
             };
 
-            //return View(viewModel); // One method of passing data to the view, is via an argument, preferred way
-            //behind the scenes, this uses:
-            //viewResult.ViewData.Model
-
-
-            //2nd method - View Dictionary
-            /*
-            ViewData["Movie"] = movie;
-            return View();
-
-            Downside to this method, (1) view code is ugly (must cast), and (2) view code is dependent on "magic string" of "Movie", makes code fragile
-            */
-
-            //3rd method -ViewBag, instead of "magic string", you have "magic object", still fragile
-            //ViewBag.Movie = movie;
-            //return View();
-
             return View(viewModel);
         }
 
-        // Section 2: Lesson 13 - Using attribute routing w/ constraints
-        /*
-         * Other possible constraints include min, max, minlength, maxlength, int, float, guid
-         * 
-         */
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Movie movie)
         {
-            return Content(year + "/" + month);
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
