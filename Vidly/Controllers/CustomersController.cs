3bf44d6777862439a48cee15650af07fc1fc285a
Will Reichl <William.Reichl@dcsg.com>
2018-10-09 13:11:06 -0400

    S7L79 - DataTables with Ajax Source - Now when we get all customers, we use an API and feed JSON to DataTables via Ajax

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -89,9 +89,7 @@ namespace Vidly.Controllers
         // GET: Customer
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-            return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
