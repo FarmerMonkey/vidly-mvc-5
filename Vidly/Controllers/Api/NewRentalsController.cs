67b1dbc98941ce39d8f32c90631f0ff7b3607510
Will Reichl <William.Reichl@dcsg.com>
2018-10-16 07:16:58 -0400

    S10L114 - Understanding the Problem - Created the skeleton for the Web API to rent the movie

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRentalsDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
