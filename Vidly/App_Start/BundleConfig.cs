60ae8cafdc3675626b7152a7f0bdf0e21f888fb5
Will Reichl <William.Reichl@dcsg.com>
2018-10-17 13:51:17 -0400

    S10L125 - Displaying Toast Notifications - Install and reference toastr.js to display notifications

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace Vidly
                         "~/Scripts/respond.js",
                         "~/Scripts/DataTables/jquery.datatables.js",
                         "~/Scripts/DataTables/datatables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -30,7 +31,8 @@ namespace Vidly
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/DataTables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
-                      "~/Content/site.css"
+                      "~/Content/site.css",
+                      "~/Content/toastr.css"
                       ));
         }
     }
